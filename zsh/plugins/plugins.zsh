ca650cc7bef672093610496e5a45aafc20aa1c2a
Daniel <info@danielripoll.es>
2015-09-24 16:59:38 +0200

    Feat: Added detector for syntax-highlight, otherwise not working
    --
    Daniel Ripoll
    danielripoll.es
    Web & Desktop Developer

---
+++ zsh/plugins/plugins.zsh
@@ -0,0 +1,3 @@
+if [ -f ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+    source ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
