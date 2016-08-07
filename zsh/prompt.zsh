a0a7d5d05e616eca020438dfbba7de3ca0fb0f56
Daniel Ripoll <info@danielripoll.es>
2016-08-07 16:03:40 +0200

    Feat: Add prompt file.
    ---
    Daniel Ripoll
    I was a DevOps before it was cool.
    http://danielripoll.es
    +34 6688 27490

---
+++ zsh/prompt.zsh
@@ -0,0 +1,27 @@
+# cheers, @ehrenmurdick
+# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+
+ruby_version() {
+  if (( $+commands[rbenv] ))
+  then
+    echo "$(rbenv version | awk '{print $1}')"
+  fi
+
+  if (( $+commands[rvm-prompt] ))
+  then
+    echo "$(rvm-prompt | awk '{print $1}')"
+  fi
+}
+
+rb_prompt() {
+  if ! [[ -z "$(ruby_version)" ]]
+  then
+    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
+  else
+    echo ""
+  fi
+}
+
+directory_name() {
+  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+}
