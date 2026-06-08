d8e7f75d0fc8b292697c915a69c4bb56ad66fb20
Daniel Ripoll <i@danielripoll.es>
2026-06-08 20:52:01 +0200

    chore: remove virtualenvwrapper and retropie leftovers

--- bash/retropie.bash
+++
@@ -1,8 +0,0 @@
-retropie_welcome() {
-    DOTFILES_LOGO_NO_AUTO=1
-    if [ -r "${DOTFILES:-$HOME/.dotfiles}/shell/logo.sh.symlink" ]; then
-        . "${DOTFILES:-$HOME/.dotfiles}/shell/logo.sh.symlink"
-        dotfiles_logo
-    fi
-    unset DOTFILES_LOGO_NO_AUTO
-}
